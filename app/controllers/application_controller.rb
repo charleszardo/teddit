f7e32738d4b498f73f4e56de0b02872b57b38f72
Dave Charnuska <davecharnuska@gmail.com>
2016-12-22 01:52:09 -0500

    add helper methods, refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
+  helper_method :logged_in?
+  helper_method :current_user
+
   def login_user!(user)
-    user.reset_session_token!
-    session[:session_token] = user.session_token
+    session[:session_token] = user.reset_session_token!
   end
 
   def logout_user!(user)
@@ -18,6 +20,6 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    !!current_user
+    !current_user.nil?
   end
 end

8800f9bd57d6d3bf7080b7c2ec7b16ef069e49fd
Dave Charnuska <davecharnuska@gmail.com>
2016-12-22 01:32:39 -0500

    change generate_session_token to generate_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   end
 
   def reset_session_token!
-    self.session_token = generate_session_token
+    self.session_token = generate_token
     self.session_token
   end
 
@@ -28,6 +28,6 @@ class User < ActiveRecord::Base
   end
 
   def ensure_session_token
-    self.session_token ||= generate_session_token
+    self.session_token ||= generate_token
   end
 end

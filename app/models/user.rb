28225b3045cd47a121c5024d35d5b5ad9a148545
Dave Charnuska <davecharnuska@gmail.com>
2016-12-22 02:16:37 -0500

    add #is_password?, save session_token after resetting

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,13 @@ class User < ActiveRecord::Base
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
   def reset_session_token!
     self.session_token = generate_token
+    self.save
     self.session_token
   end
 

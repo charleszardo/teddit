a1b6d6902e23d333f0875a8d815a97b667e34257
Dave Charnuska <davecharnuska@gmail.com>
2016-12-22 01:36:38 -0500

    add auth methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  def login_user!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def logout_user!(user)
+    session[:session_token] = nil
+    user.reset_session_token!
+  end
+
+  def current_user
+    session[:session_token] ? User.find_by_session_token(session[:session_token]) : nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end

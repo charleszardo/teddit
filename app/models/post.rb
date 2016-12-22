f63b1d3a6698f0bded495265a85c8fa54eedbb2a
Dave Charnuska <davecharnuska@gmail.com>
2016-12-22 16:25:01 -0500

    add posts show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
 
   belongs_to :sub
   belongs_to :author, class_name: "User"
+
+  def author_name
+    self.author.username
+  end
+
+  def sub_name
+    self.sub.title
+  end
+
+  def is_owner?(user)
+    self.author == user
+  end
 end

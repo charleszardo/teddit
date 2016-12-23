8dde6538fc8d2f27f2abed167291f58bf0e79df5
Dave Charnuska <davecharnuska@gmail.com>
2016-12-23 01:58:47 -0500

    add parent_comment_id to comments table, associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ActiveRecord::Base
 
   belongs_to :author, class_name: "User"
   belongs_to :post
+  belongs_to :parent_comment, class_name: "Comment", foreign_key: :parent_comment_id
+
+  has_many :child_comments, class_name: "Comment", foreign_key: :parent_comment_id
 end

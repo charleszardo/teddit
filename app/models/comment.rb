09af75026d6671a7bbdbd6ae33a9ddbba33652d9
Dave Charnuska <davecharnuska@gmail.com>
2016-12-23 20:07:56 -0500

    add annotate gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id                :integer          not null, primary key
+#  content           :text             not null
+#  author_id         :integer          not null
+#  post_id           :integer          not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  parent_comment_id :integer
+#
+
 class Comment < ActiveRecord::Base
   validates :content, :author, presence: true
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id           :integer          not null, primary key
+#  value        :integer          not null
+#  voter_id     :integer          not null
+#  votable_id   :integer
+#  votable_type :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Vote < ActiveRecord::Base
   validates :value, :voter_id, :votable_id, :votable_type, presence: true
   validates :voter_id, :uniqueness => {:scope => [:votable_id, :votable_type]}

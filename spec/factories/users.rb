cd5942862e8f363a6e8fa82ab75802b40a011595
Dave Charnuska <davecharnuska@gmail.com>
2017-01-10 00:56:52 -0500

    add more auth-related tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    username "test_user"
+    password "abcdef"
+    email 'xyz@example.com'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,25 @@ RSpec.describe User, type: :model do
 
   describe "password encryption" do
     it "does not save passwords to the database" do
-      User.create!(username: "test_user", password: "abcdef", email: 'xyz@example.com')
-      user = User.find_by_username("test_user")
-      expect(user.password).not_to be("abcdef")
+      user = create(:user)
+      retrieved_user = User.find_by_username(user.username)
+      expect(retrieved_user.password).not_to be("abcdef")
     end
 
     it "encrypts the password using BCrypt" do
       expect(BCrypt::Password).to receive(:create)
-      User.new(username: "test_user", password: "abcdef")
+      build(:user)
+    end
+
+    describe "::find_by_username_and_password" do
+      it "returns user if credentials are correct" do
+        user = create(:user)
+        expect(User.find_by_username_and_password("test_user", "abcdef")).to eq(user)
+      end
+
+      it "returns nil if credentials are incorrect" do
+
+      end
     end
   end
 

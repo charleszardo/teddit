b91b03e2761b408fef0c2217462df480411c584f
Dave Charnuska <davecharnuska@gmail.com>
2017-02-13 00:30:28 -0500

    add auth methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,42 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
+
+  def require_login
+    unless user_signed_in?
+      # redirect_to root_url
+    end
+  end
+
+  def require_no_login
+    if user_signed_in?
+      # redirect_to root_url
+    end
+  end
+
+  def require_owner
+    item = Object.const_get(controller_name.classify).find(params[:id])
+    unless user_signed_in? && current_user.owns?(item)
+      # redirect_to root_url
+    end
+  end
+
+  def vote(value)
+    vote_hash = { voter_id: current_user.id,
+                  votable_id: params[:id],
+                  votable_type: params[:controller].classify }
+
+    vote = Vote.find_by(vote_hash)
+    vote ? update_vote(vote, value) : new_vote(vote_hash, value)
+  end
+
+  def update_vote(vote, value)
+    vote.value = vote.value == value ? 0 : value
+    vote.save
+  end
+
+  def new_vote(vote_hash, value)
+    vote_hash[:value] = value
+    Vote.create!(vote_hash)
+  end
 end

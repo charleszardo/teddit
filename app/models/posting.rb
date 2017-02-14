07a7af74bdefcdfca1970041e8d6a5674a9f079f
Dave Charnuska <davecharnuska@gmail.com>
2017-02-14 00:31:42 -0500

    add posting associations, validations

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,2 +1,7 @@
 class Posting < ActiveRecord::Base
+  validates :post, :sub, presence: true
+  validates :post, uniqueness: { scope: :sub_id }
+
+  belongs_to :post, inverse_of: :postings
+  belongs_to :sub, inverse_of: :postings
 end
